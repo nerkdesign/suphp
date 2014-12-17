4488e1617f24bc3bad9cd95cf3eb7f8f5b3eb465
nerkdesign <pporcheret@free.fr>
2014-12-17 13:00:12 +0100

    creationbase.php
    Script de creation de la table en base de donnée
    
    Pour la jouer, remplacez les valeurs dans le script puis jouer le en faisant : #php5 ./creationbase.php
    
    A remplacer :
    serveur.sql : ip ou nom du serveur mysql
    user : utilisateur mysql
    sesame : son mot de passe
    base_sql : nom de la base sql a utiliser

---
+++ creationbase.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Script de creation de la table en base de donnée
+
+Pour la jouer, remplacez les valeurs dans le script puis jouer le en faisant : #php5 ./creationbase.php
+
+A remplacer :
+serveur.sql : ip ou nom du serveur mysql
+user : utilisateur mysql
+sesame : son mot de passe
+base_sql : nom de la base sql a utiliser
+*/
+
+mysql_connect("serveur.sql", "user", "sesame") or die(mysql_error());
+	mysql_select_db("base_sql") or die(mysql_error());
+
+	mysql_query("CREATE TABLE supervision (
+		id INT AUTO_INCREMENT,
+		nom TEXT,
+		ip TEXT,
+		url TEXT,
+		t_ssh INT,
+		t_url INT,
+		t_ping INT,
+	  r_ssh INT,
+		r_url INT,
+		r_ping INT
+	
+	  PRIMARY KEY(id)
+	)") Or die(mysql_error());
+	mysql_close ();
+	
+?>
